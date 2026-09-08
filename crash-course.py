03ef9b226061653d5bb1166bdaa82019d1227906
garretthunt <garretthunt118@gmail.com>
2026-09-08 18:22:20 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,35 @@
+# comment
+
+# here
+# is
+# a
+# comment
+
+print("Hello, World!")
+
+# VARIABLE DECLARATIONS:
+
+a = 4           # integer
+b = 5.5         # float
+c = "CSAEA"     # string
+d = False       # boolean
+
+print(a, b, c, d)
+
+# OPERATORS:
+# + - / *    %  **  //
+# +=   -=   /=
+
+e = 3 - 1
+print(e)
+e += 7
+print(e)
+
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 7
+e += 12
+
+print(f"e is NOW equal to {e}")
