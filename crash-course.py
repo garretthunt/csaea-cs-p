69300c3c92d8876d15b37a2ea2b126467c79c61e
garretthunt <garretthunt118@gmail.com>
2026-09-10 12:27:56 -0400

    crash-course.py

--- crash-course.py
+++ crash-course.py
@@ -1,35 +1,82 @@
+import math
+
 # comment
 
-# here
-# is
-# a
-# comment
+# to comment multiple lines,
+# highlight the lines,
+# and click ctrl + / 
 
-print("Hello, World!")
+print("Hello world!")
 
-# VARIABLE DECLARATIONS:
+# VARIABLE DECLARATIONS AND DATA TYPES:
 
-a = 4           # integer
-b = 5.5         # float
+a = 4           # integer 
+b = 5.5         # float 
 c = "CSAEA"     # string
-d = False       # boolean
+d = False       # boolean 
 
+print(a)
 print(a, b, c, d)
 
-# OPERATORS:
+# MATH OPERATORS 
 # + - / *    %  **  //
-# +=   -=   /=
+# +=   -=   /=   
 
 e = 3 - 1
 print(e)
-e += 7
+e += 20
 print(e)
 
-# f-string
+# f-string, formatted strings
 
 print(f"e is equal to {e}")
 
-e -= 7
+e -= 7 
 e += 12
 
 print(f"e is NOW equal to {e}")
+
+# COMPARISONS (booleans, which always return True of False)
+
+#  <   >    <=   >=    ==    !=
+
+print(5 <=  5)
+print(7 == 4)
+print(1 != 2)
+
+isEqual = "Yes" != "YES"
+print(isEqual)
+
+# LOGICAL OPERATORS
+# In order of precedence: not   and   or
+
+f = False
+t = True
+
+#predict output. DOn't run!
+print(not f) # True
+print( f and t) #False
+print(f or t) #True
+print(f or t and not f) # True
+
+# CASTING ()
+
+g = int(5.6536539)
+print(g)
+
+# STRINGS
+
+s1 = "Goodnight"
+s2 = " and "
+s3 = "Goodbye"
+end = s1 + s2+ s3 # concatenation with +
+end += ", Cowboy."
+
+print(end + "\n")
+
+# MATH LIBRARY
+
+print(math.sqrt(14))
+print(math.ceil(3.65))
+print(math.floor(8.94))
+print(math.pow(2, 4))
