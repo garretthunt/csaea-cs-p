eb6c0c44e864d1d6aa2de9b9112435b7ffea1d13
garretthunt <garretthunt118@gmail.com>
2026-09-11 13:19:41 -0400

    crash-course.py
    Added examples of list indexing and methods.

--- crash-course.py
+++ crash-course.py
@@ -62,6 +62,7 @@ print(f or t and not f) # True
 # CASTING ()
 
 g = int(5.6536539)
+h = str(54) # changes int 54 to string "54"
 print(g)
 
 # STRINGS
@@ -80,3 +81,60 @@ print(math.sqrt(14))
 print(math.ceil(3.65))
 print(math.floor(8.94))
 print(math.pow(2, 4))
+
+# CONDITIONALS
+
+# if    elif    else 
+
+t = True
+f = False
+
+if 1 != 2: 
+    print("Reached the first condition")
+else: 
+    print("Reached else")
+  else: 
+    print("Reached else")
+
+
+if 1 > 1 and 1 == 1: 
+    print("Reached the first condition")
+elif 6 == 7 or 2 != 3:
+    print("Reached second condition")
+elif 9 != 10:
+  print("Reached thrid condition")
+else: 
+    print("Reached else")
+
+# LISTS
+# A list can hold any type, and can grow or shrink at any time.
+
+#index: 0   1   2   3   4
+nums = [34, 52, 3, 64, 32]
+
+print(nums)
+print(nums[3]) #predict
+print(nums[0])
+print(nums[-1])
+print(nums[-3])
+
+print(nums[0] + nums[2])
+nums[0] = 64
+print(nums)
+
+# LIST METHODS:
+# Special built-in methods
+
+words = []
+
+words.append("Word 1")
+words.append("Word 2")
+words.append("Word 3")
+print(words)
+
+words.remove("Word 1")
+words.insert(0, "Word 4")
+words[1] = "Word 5"
+length = len(words)
+print(words)
+print(length)
